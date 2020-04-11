fd77cdb58a8b0adec5bdb3ece63ad044b0d22c4f
CHISOM96 <chisomnnabuihe@gmail.com>
2020-04-11 11:06:00 +0100

    Completed challenge 1 requirements

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,50 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+    const { region,
+        reportedCases,
+        periodType,
+        timeToElapse,
+        totalHospitalBeds
+    } = data;
+    //Challenge 1
 
+    //Impact computation
+
+    const impact = {
+        currentlyInfected: reportedCases * 10
+    };
+
+    //Severe Impact 
+
+    const severeImpact = {
+        currentlyInfected: reportedCases * 50
+    };
+    //Infection rate
+
+    const durationCheck = (periodType, duration) => {
+        let infectionRate = 2 ** Math.floor(duration / 3);
+        if (periodType === 'weeks') {
+            infectionRate = 2 ** Math.floor((duration * 7) / 3);
+        } else if (periodType === 'months') {
+            infectionRate = 2 ** Math.floor((duration * 30) / 3);
+        }
+        return infectionRate;
+    };
+    /*const durationCheck = (periodType, duration) => {
+    let infectionRate = Math.pow(2, Math.floor(duration / 3));
+    if (periodType === 'weeks') {
+        infectionRate = Math.pow(2, Math.floor(duration * 7 / 3));
+    } else if (periodType === 'months') {
+        infectionRate = Math.pow(2, Math.floor(duration * 30 / 3));
+    }
+    return infectionRate;
+    };
+    This also works*/
+
+    const getDuration = durationCheck(periodType, timeToElapse);
+
+    impact.infectionsByRequestedtime = impact.currentlyInfected * getDuration;
+    severeImpact.infectionsByRequestedtime = severeImpact.currentlyInfected * getDuration;
+
+
+}
 export default covid19ImpactEstimator;
